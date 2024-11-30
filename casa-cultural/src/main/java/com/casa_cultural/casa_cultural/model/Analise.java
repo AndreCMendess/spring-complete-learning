a4abb225cd4f5855f8fe30f34cc3c07b8afd0ef3
AndreCMendess <andremendesdede2@gmail.com>
2024-11-30 15:09:35 -0300

    Criando entidades model filme e analise

---
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Analise.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.casa_cultural.casa_cultural.model;
+
+/**
+ *
+ * @author user
+ */
+public class Analise {
+    
+}
---
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Filme.java
@@ -0,0 +1,54 @@
+
+package com.casa_cultural.casa_cultural.model;
+
+
+public class Filme {
+    private int id;
+    private String titulo;
+    private String genero;
+    private String anoLancamento;
+
+    public Filme(int id, String titulo, String genero, String anoLancamento) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public Filme() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+    
+    
+}
