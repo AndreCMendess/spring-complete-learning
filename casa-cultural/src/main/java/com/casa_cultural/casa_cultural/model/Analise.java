1e9ea69bbf67d1879bb82a594c5fb033a171c668
AndreCMendess <andremendesdede2@gmail.com>
2024-12-09 22:37:02 -0300

    Transformacao das classes do model em entidades JPA e mapeamento do relacionamento entre Filme e Analise

--- casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Analise.java
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Analise.java
@@ -1,51 +1,19 @@
 package com.casa_cultural.casa_cultural.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Analise")
 public class Analise {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name ="filme_id")
     private Filme filme;
     private String analise;
     private double nota;
-
-    public Analise(int id, Filme filme, String analise, double nota) {
-        this.id = id;
-        this.filme = filme;
-        this.analise = analise;
-        this.nota = nota;
-    }
-
-    public String getAnalise() {
-        return analise;
-    }
-
-    public void setAnalise(String analise) {
-        this.analise = analise;
-    }
-
-    public Analise() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Filme getFilme() {
-        return filme;
-    }
-
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
-
-    public double getNota() {
-        return nota;
-    }
-
-    public void setNota(double nota) {
-        this.nota = nota;
-    }
-       
+    
 }
--- casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Filme.java
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/model/Filme.java
@@ -1,91 +1,24 @@
 
 package com.casa_cultural.casa_cultural.model;
 
-import org.springframework.stereotype.Component;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Data
+@Entity
+@Table(name="Analise")
 public class Filme {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String titulo;
     private String sinopse;
     private String genero;
     private String anoLancamento;
+    @OneToMany(mappedBy = "filme" ,cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Analise> analise = new ArrayList();
 
-    public Filme(int id, String titulo, String sinopse, String genero, String anoLancamento) {
-        this.id = id;
-        this.titulo = titulo;
-        this.sinopse = sinopse;
-        this.genero = genero;
-        this.anoLancamento = anoLancamento;
-    }
-
-    public List<Analise> getAnalise() {
-        return analise;
-    }
-
-    public void setAnalise(List<Analise> analise) {
-        this.analise = analise;
-    }
-
-    
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    } 
-
-    public Filme() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(String anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-    
-    public double mediaNota(){
-        double totalAnalise = 0;
-        double media = 0;
-        
-        for(int i=0; i<analise.size(); i++){
-            totalAnalise  += analise.get(i).getNota();
-            media = totalAnalise / analise.size();
-        }
-        
-        return media; 
-    }
-    
 }
