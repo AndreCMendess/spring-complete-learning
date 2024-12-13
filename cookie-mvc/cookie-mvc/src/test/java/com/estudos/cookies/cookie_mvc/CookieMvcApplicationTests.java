6af3a1476430d847a88e0e9652b0291277a04d75
AndreCMendess <andremendesdede2@gmail.com>
2024-12-12 21:29:22 -0300

    iniciando projeto de estudos sobre cookies

---
+++ cookie-mvc/cookie-mvc/src/test/java/com/estudos/cookies/cookie_mvc/CookieMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.cookies.cookie_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
