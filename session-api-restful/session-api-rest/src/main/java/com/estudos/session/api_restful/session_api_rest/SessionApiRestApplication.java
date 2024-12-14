851720dd4f9c299ddc5d88a546fb08eaf43daf23
AndreCMendess <andremendesdede2@gmail.com>
2024-12-14 16:55:02 -0300

    criando projeto spring , para estudos de session em um projeto restful

---
+++ session-api-restful/session-api-rest/src/main/java/com/estudos/session/api_restful/session_api_rest/SessionApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.session.api_restful.session_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionApiRestApplication.class, args);
+	}
+
+}
---
+++ session-api-restful/session-api-rest/src/test/java/com/estudos/session/api_restful/session_api_rest/SessionApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.session.api_restful.session_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
