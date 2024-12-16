c0a3a10227d9da7ecfa216a3ca7d540089851880
AndreCMendess <andremendesdede2@gmail.com>
2024-12-15 23:14:55 -0300

    Refatorando RestFilmeController para utilizar ResponseEntity em todas as respostas

--- casa-cultural/src/main/java/com/casa_cultural/casa_cultural/controller/RestFilmeController.java
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/controller/RestFilmeController.java
@@ -3,6 +3,8 @@ package com.casa_cultural.casa_cultural.controller;
 import com.casa_cultural.casa_cultural.model.Filme;
 import com.casa_cultural.casa_cultural.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +18,33 @@ public class RestFilmeController {
     FilmeService filmeService;
 
     @GetMapping
-    public List<Filme>buscarFilmes() {
-        return filmeService.getTodosFilmes();
+    public ResponseEntity<List>buscarFilmes() {
+        List<Filme> filmes = filmeService.getTodosFilmes();
+        return new ResponseEntity<>(filmes, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Filme buscarFilmePorId(@PathVariable Integer id) {
-        return filmeService.getFilmePorId(id);
+    public ResponseEntity<Filme> buscarFilmePorId(@PathVariable Integer id) {
+        Filme filme = filmeService.getFilmePorId(id);
+        return new ResponseEntity<>(filme,HttpStatus.OK);
     }
 
     @PostMapping("/adicionar")
-    public Filme adicionarFiflme(@RequestBody Filme novoFilme) {
-        return filmeService.adicionandoFilme(novoFilme);
+    public ResponseEntity<Filme> adicionarFilme(@RequestBody Filme filme) {
+        var novoFilme = filmeService.adicionandoFilme(filme);
+        return new ResponseEntity<>(novoFilme,HttpStatus.OK);
     }
 
     @PutMapping("/filmes/{id}")
-    public Filme atualizarFilme(@PathVariable Integer id , @RequestBody Filme filmeAtualizado) {
-        return filmeService.atualizarFilme(id,filmeAtualizado);
+    public ResponseEntity<Filme> atualizarFilme(@PathVariable Integer id , @RequestBody Filme filme) {
+        var filmeAtualizado = filmeService.atualizarFilme(id,filme);
+        return new ResponseEntity<>(filmeAtualizado,HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public boolean  deletarFilme(@PathVariable Integer id) {
-        return filmeService.deletarFilme(id);
+    public ResponseEntity deletarFilme(@PathVariable Integer id) {
+        filmeService.deletarFilme(id);
+        return new ResponseEntity(HttpStatus.OK);
 
     }
 
