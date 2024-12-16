e19e41802fb18342e878867ffeef40c31585d18c
AndreCMendess <andremendesdede2@gmail.com>
2024-12-15 22:03:24 -0300

    Refatorar rotas e métodos de gerenciamento de filmes e analises
    - Alterar URL para listagem de filmes de "/lista-filmes" para "/filmes".
    - Corrigir lógica para adicionar ou atualizar analise de filme com a rota "/filmes/{id}/analise".
    - Ajustar rota de atualizacao de filme para "/filmes/{id}/atualizar".
    - Garantir redirecionamento adequado apas adição ou atualização de filmes e analises.
    Refatorar rotas e metodos de gerenciamento de filmes e analises
    
    - Alterar URL para listagem de filmes de "/lista-filmes" para "/filmes".
    - Corrigir lógica para adicionar ou atualizar analise de filme com a rota "/filmes/{id}/analise".
    - Ajustar rota de atualizacao de filme para "/filmes/{id}/atualizar".
    - Garantir redirecionamento adequado apas adição ou atualizacao de filmes e analises.

--- casa-cultural/src/main/java/com/casa_cultural/casa_cultural/controller/CasaCulturalController.java
+++ casa-cultural/src/main/java/com/casa_cultural/casa_cultural/controller/CasaCulturalController.java
@@ -1,5 +1,6 @@
 package com.casa_cultural.casa_cultural.controller;
 
+import com.casa_cultural.casa_cultural.model.Analise;
 import com.casa_cultural.casa_cultural.model.Filme;
 import com.casa_cultural.casa_cultural.service.AnaliseService;
 import com.casa_cultural.casa_cultural.service.FilmeService;
@@ -16,36 +17,68 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class CasaCulturalController {
     @Autowired
     FilmeService filmeService;
+    @Autowired
     AnaliseService analiseService;
 
     @GetMapping("/")
     public String paginaInicial(Model model){
         double media= 0;
 
-        model.addAttribute("listaFilmes",filmeService.getTodosFilmes());
+        model.addAttribute("listarFilmes",filmeService.getTodosFilmes());
 
         model.addAttribute("mediaNota",media);
         return "pagina-inicial";
     }
 
-    @GetMapping("/deletarFilme/{id}")
-    public String deletarFilme(@PathVariable(value="id")Integer id){
-        filmeService.deletarFilme(id);
+    @GetMapping("/lista-filmes")
+    public String listarFilmesEanalises(Model model){
+        Analise analise = new Analise();
+        model.addAttribute("listarFilmes",filmeService.getTodosFilmes());
+        model.addAttribute("analise",analise);
+        return "filmes";
+    }
+
+    @PostMapping("/lista-filmes")
+    public String adicionarAnalise(@ModelAttribute("analise") Analise analise, BindingResult result){
+        if(result.hasErrors()){
+            return "filmes";
+        }
+
+
+        if(analise.getId() == null){
+            analiseService.adicionarAnalise(analise,analise.getFilme().getId());
+        }else{
+            analiseService.atualizarAnalise(analise.getId(),analise);
+        }
         return "redirect:/";
     }
 
-    @GetMapping("/criarFilmeForm")
+
+    @GetMapping("/deletarFilme/{id}")
+    public String deletarFilme(@PathVariable(value="id") Integer id){
+        try {
+
+            filmeService.deletarFilme(id);
+            return "filmes";
+        } catch (NumberFormatException e) {
+            return "redirect:/erro";
+        }
+    }
+
+    @GetMapping("/cadastrar-filme")
     public String criarFilmeForm(Model model){
         Filme filme = new Filme();
         model.addAttribute("filme",filme);
         return "cadastrar-filme";
     }
 
+
     @PostMapping("/cadastrar-filme")
     public String adicionarFilme(@ModelAttribute("filme") Filme filme, BindingResult result){
         if(result.hasErrors()){
             return "cadastrar-filme";
         }
+
 
         if(filme.getId() == null){
             filmeService.adicionandoFilme(filme);
@@ -55,11 +88,22 @@ public class CasaCulturalController {
         return "redirect:/";
     }
 
-    @GetMapping("/atualizarFilmeForm")
+    @GetMapping("/atualizarFilmeForm/{id}")
     public String atualizarFIlme(@PathVariable(value="id") Integer id,Model model){
         Filme filme = filmeService.getFilmePorId(id);
         model.addAttribute("filme",filme);
-        return "atualizar";
+        return "atualizar-filme";
+    }
+
+    @PostMapping("/atualizarFilmeForm/{id}")
+    public String atualizarFilme(@PathVariable(value="id") Integer id, @ModelAttribute Filme filme, BindingResult result){
+        if(result.hasErrors()) {
+            return "atualizar-filme";
+        }
+
+        filme.setId(id);
+        filmeService.atualizarFilme(id, filme);
+        return "redirect:/lista-filmes";
     }
 
 
